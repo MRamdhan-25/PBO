fdcb488243e0e99877aabae75d34c09077239317
MRamdhan-25 <mandzonswag@gmail.com>
2025-08-30 11:30:38 +0700

    Add files via upload

---
+++ NetBeansProjects/Rama_PBO2/src/main/java/com/mycompany/rama_pbo2/Rama_PBO2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rama_pbo2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Rama_PBO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Muhammad Ramdhan");
+        System.out.println("NPM: 2420506027");
+        System.out.println("Alamat: Bangsari ");
+        System.out.println("No_HP: 087834995406");
+    }
+}
