e0ad69b9a9cd1ea49d97c8b437a14c0061600549
MRamdhan-25 <mandzonswag@gmail.com>
2025-11-04 17:06:00 +0700

    Add files via upload

---
+++ NetBeansProjects/Praktikum9/Tugas/TugasModul9/Anjing.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TugasModul9;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Anjing extends Hewan{
+    @Override
+    void bersuara() {
+        System.out.println("Anjing mengeluarkan suara: GukGuk");
+    }
+    
+}
---
+++ NetBeansProjects/Praktikum9/Tugas/TugasModul9/Kucing.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TugasModul9;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Kucing extends Hewan{
+    @Override
+    void bersuara() {
+        System.out.println("Kucing menngeluarkan suara: Meong");
+    }
+    
+}
